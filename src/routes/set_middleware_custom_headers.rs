d695035a67d187c15b84d85fded5ed550153eaff
Caden <cadenrothzeid@gmail.com>
2023-02-02 10:26:06 -0700

    spelling

--- src/routes/set_middleware_custom_headers.rs
+++ src/routes/set_middleware_custom_headers.rs
@@ -7,7 +7,7 @@ pub async fn set_middleware_custom_headers<B>(mut request: Request<B>, next:Next
     let headers = request.headers();
     let message = headers.get("message").ok_or(StatusCode::BAD_REQUEST )?;
     let message = message.to_str().map_err(|_| StatusCode::BAD_REQUEST)?.to_string();
-    let extention = request.extensions_mut();
-    extention.insert(HeaderMessage(message));
+    let extension = request.extensions_mut();
+    extension.insert(HeaderMessage(message));
     Ok(next.run(request).await)
 }
